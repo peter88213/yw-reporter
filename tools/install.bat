6b451aea2104b6da82be0d826e270989a24d633d
peter88213 <peter.88213@t-online.de>
2021-12-26 10:16:28 +0100

    Rebuild v1.4.3 with fixed installation script.

--- tools/install.bat
+++ tools/install.bat
@@ -1,14 +1,20 @@
 set app=yw-reporter
+
 set oldappdir="%APPDATA%\PyWriter\%app%"
 set basedir="%USERPROFILE%\.pywriter"
 if not exist %basedir% md %basedir%
-set appdir="%basedir%\%app%"
-if exist %oldappdir% move %oldappdir% %appdir%
+set appdir="%USERPROFILE%\.pywriter\%app%"
+
+if not exist %oldappdir% goto label1 
+move %oldappdir% %appdir%
+if exist %USERPROFILE%\Desktop\%app%.lnk del %USERPROFILE%\Desktop\%app%.lnk
+
+:label1
 if not exist %appdir% md %appdir%
 
 copy %app%.pyw %appdir%
 
-set cnfdir="%appdir%\config"
+set cnfdir="%USERPROFILE%\.pywriter\%app%\config"
 if not exist %cnfdir% md %cnfdir%
 
 rem echo "N" | copy/-Y sample\*.* %cnfdir%
@@ -21,7 +27,7 @@ echo The %app% program is installed.
 echo Now create a shortcut on your desktop. 
 echo For this, hold down the Alt key on your keyboard and then drag and drop %app%.pyw to your desktop. 
 @echo off
-explorer "%appdir%\"
+explorer %appdir%\
 pause
 
 :end

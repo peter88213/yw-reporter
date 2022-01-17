0078cda3f1bba03ff64d69ea6c0caebf4a7496bc
peter88213 <peter.88213@t-online.de>
2022-01-17 14:29:38 +0100

    Update docstring

---
+++ src/ywreporter/__init__.py
@@ -0,0 +1,12 @@
+"""Package for processing Scapple files.
+
+Modules:
+
+scap_file -- Provide a class for Scapple file representation.
+scap_note -- Provide a class for Scapple note representation.
+scap_converter -- Provide a Scapple converter class for Scapple diagram import.
+
+Copyright (c) 2022 Peter Triesberger
+For further information see https://github.com/peter88213/scappex
+Published under the MIT License (https://opensource.org/licenses/mit-license.php)
+"""
